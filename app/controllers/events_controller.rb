0711904b9a14e1b7e8b6ce065a905bb19ad50187
Julia Park <juliapark91@gmail.com>
2015-05-25 18:15:28 -0700

    created index of events page, must fix new events page

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,69 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
+  def new
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+    @event = current_user.events.build( event_params )
+    authorize @event
+
+    if @event.save
+      redirect_to @event, notice: "Application was registered successfully."
+    else
+      flash[:error] = "Error registering application. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    authorize @event
+
+    if @event.update_attributes( event_params )
+      redirect_to @event
+    else
+      flash[:error] = "Error updating applcation. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    name = @event.name
+    authorize @event
+
+    if @event.destroy
+      flash[:notice] = "\"#{name}\" was deleted successfully."
+    else
+      flash[:error] = "Error deleting application. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+private
+
+  def event_params
+    params.require(:event).permit(:name, :url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :events
+
   devise_for :users
 
   get "welcome/index"
