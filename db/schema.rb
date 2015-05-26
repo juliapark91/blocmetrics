d6a9646e9af0b5266e6cc247a25f06948a4e097a
Julia Park <juliapark91@gmail.com>
2015-05-26 15:56:32 -0700

    created events model and faker data

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526010614) do
+ActiveRecord::Schema.define(version: 20150526220338) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "url"
     t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "registered_application_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_index "events", ["user_id"], name: "index_events_on_user_id"
+  add_index "events", ["registered_application_id"], name: "index_events_on_registered_application_id"
 
   create_table "registered_applications", force: :cascade do |t|
     t.string   "url"
