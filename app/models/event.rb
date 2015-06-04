a00b84c779e874f2fdd9239f1ddb6986fec21721
Conrad Taylor <conradwt@gmail.com>
2015-06-04 01:58:24 -0700

    Add schema information to the models.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  name       :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Event < ActiveRecord::Base
   belongs_to :registered_application
   has_many :users, through: :registered_applications
--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: registered_applications
+#
+#  id         :integer          not null, primary key
+#  url        :string
+#  name       :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class RegisteredApplication < ActiveRecord::Base
   belongs_to :user
   has_many :events, dependent: :destroy
