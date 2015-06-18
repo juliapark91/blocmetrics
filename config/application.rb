f00fc8d1cd2548c66c1ae5aaaf0655b5c76b8746
Julia Park <juliapark91@gmail.com>
2015-06-18 09:06:57 -0700

    added rack cors to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bloccit
+module Blocmetrics
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -30,7 +22,7 @@ module Bloccit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+   
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger } ) do
       allow do
         origins '*'
@@ -41,5 +33,7 @@ module Bloccit
           :max_age => 0
       end
     end
+    #getting DevelopmentMailInterceptor to work
+    config.autoload_paths += %W(#{Rails.root}/lib)
   end
 end
